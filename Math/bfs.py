997b665ea4a56ba980da91b849e63e7e427d59c3
Gobi03 <t.katsushima@outlook.jp>
2016-02-17 02:15:45 +0900

    update

---
+++ Math/bfs.py
@@ -0,0 +1,12 @@
+que = []
+que.append()
+while(not que.isEmpty()):
+	now = que.pop(0)
+	
+	## process to now
+
+	# enqueue
+	for i in range():
+		nex = i-th statement
+		if(condition):
+			que.enqueue(nex)
