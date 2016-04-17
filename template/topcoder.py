cbb8c48a893cdf504548b1c76cea8271792e32f8
Gobi03 <t.katsushima@outlook.jp>
2016-04-18 00:23:35 +0900

    update

--- template/topcoder.py
+++ template/topcoder.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 def makelist(n, m):
 	return [[0 for i in range(m)] for j in range(n)]
 
---
+++ 基本操作/Stack.py
@@ -0,0 +1,5 @@
+stack = [3, 4, 5]
+stack.append(6)
+stack.pop()
+6
+len(stack) == 0    # empty()
