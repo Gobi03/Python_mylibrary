88102735453ae990e912f6d8c7cec41044e42450
Gobi03 <t.katsushima@outlook.jp>
2016-05-13 06:59:01 +0900

    update

--- Math/繰り返し二乗法.py
+++ Math/繰り返し二乗法.py
@@ -2,12 +2,12 @@
 
 MOD = 1000000007
 def repeatSquares(x, n):  # x^n
-  ans = 1
-  while n != 0:
-    if n % 2 == 1:
-      ans *= x
-      ans %= MOD
-    x *= x
-    n //= 2
-    x %= MOD      # MOD演算ないなら消していい
-  return ans
+	res = 1
+	while n != 0:
+		if n % 2 == 1:
+			res *= x
+			res %= MOD  # MOD演算ないなら消していい
+		x *= x
+		n //= 2
+		x %= MOD      # MOD演算ないなら消していい
+	return res
