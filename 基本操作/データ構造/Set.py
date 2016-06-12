4aeb792f0d82d6a99848ce38238f34ec5ccdb705
Gobi03 <t.katsushima@outlook.jp>
2016-06-12 20:32:28 +0900

    update

--- 基本操作/データ構造/Set.py
+++ 基本操作/データ構造/Set.py
@@ -3,16 +3,13 @@ a = set(['red', 'blue', 'green'])
 b = set(['green', 'yellow', 'white'])
 
 # 要素の追加
-a.add(['black'])
+a.add('black')
 
-# 差集合
-print a - b            #=> set(['red', 'blue'])
-# 和集合
-print a | b            #=> set(['red', 'blue', 'green', 'yellow', 'white'])
-# 積集合
-print a & b            #=> set(['green'])
-# XOR
-print a ^ b            #=> set(['red', 'blue', 'yellow', 'white'])
+## 集合演算
+print a - b   # 差集合
+print a | b   # 和集合
+print a & b   # 積集合
+print a ^ b   # XOR
 
 # 要素が含まれているかどうか
 print 'green' in a     #=> True
