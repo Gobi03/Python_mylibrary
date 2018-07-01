687e21f92990aecdf40849db35767d14f615842c
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2018-07-02 00:24:00 +0900

    ３分探索の小型化

--- Math/TernarySearchInt.py
+++ Math/TernarySearchInt.py
@@ -1,34 +1,17 @@
-# 要テスト
-
 # check は数値を返す
 
 # 上に凸のグラフの最大値
 def ternarySearchInt(small, big):
-    # 初期条件
-    if small == big:
-        return small
-    elif big - small == 1:
-        if check(small) >= check(big):
-            return small
-        else:
-            return big
-    
     def func(small, big):
-        midl = (big + small) // 2
-        midr = midl + 1
         if big - small == 2:  # 終了条件
-            mid = midl
-            if check(small) >= check(mid):
-                if check(small) >= check(big):
-                    return small
-                else:
-                    return big
+            mid = (big + small) // 2
+            if check(big) >= check(mid):
+                return big
             else:
-                if check(mid) >= check(big):
-                    return mid
-                else:
-                    return big
+                return small if check(small) >= check(mid) else mid
         else:
+            midl = (big + small) // 2
+            midr = midl + 1
             if check(midl) <= check(midr):
                 return func(midl, big)
             else:
@@ -38,33 +21,16 @@ def ternarySearchInt(small, big):
 
 # 下に凸のグラフの最小値
 def ternarySearchInt(small, big):
-    # 初期条件
-    if small == big:
-        return small
-    elif big - small == 1:
-        if check(small) <= check(big):
-            return small
-        else:
+    if big - small <= 2:  # 終了条件
+        mid = (big + small) // 2
+        if check(big) <= check(mid):
             return big
-
-    def func(small, big):
+        else:
+            return small if check(small) <= check(mid) else mid
+    else:
         midl = (big + small) // 2
         midr = midl + 1
-        if big - small == 2:  # 終了条件
-            mid = midl
-            if check(small) <= check(mid):
-                if check(small) <= check(big):
-                    return small
-                else:
-                    return big
-            else:
-                if check(mid) <= check(big):
-                    return mid
-                else:
-                    return big
+        if check(midl) >= check(midr):
+            return ternarySearchInt(midl, big)
         else:
-            if check(midl) >= check(midr):
-                return func(midl, big)
-            else:
-                return func(small, midr)
-    return func(small, big)
+            return ternarySearchInt(small, midr)
