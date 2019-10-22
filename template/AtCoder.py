4778a0c84742de3fb073204df44e1a31cc672dce
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2019-10-23 00:10:51 +0900

    template

--- template/AtCoder.py
+++ template/AtCoder.py
@@ -1,8 +1,10 @@
+import math
+
 def makelist(n, m):
     return [[0 for _ in range(m)] for _ in range(n)]
 
 # n = int(input())
-# a, b = map(int, input().split())
+# a, b = list(map(int, input().split()))
 # s = input()
 
 
