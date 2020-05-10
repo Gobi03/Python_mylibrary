9dda6669ef08b96c2a9f713ec1ec46dde73772bc
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-05-10 23:45:14 +0900

    二分探索を簡易化

--- Math/BinarySearch.py
+++ Math/BinarySearch.py
@@ -1,23 +1,35 @@
 # 条件を満たす最大の値を返す
-def binarySearch(small, big):
-    mid = (big + small) // 2
-    if big - small <= 1:
-        if check(big): return big
-        else:          return small
-    else:
-        if check(mid):
-            return binarySearch(mid, big)
+def binarySearch(_small, _big):
+    def check(i):
+        return True
+
+    def func(small, big):
+        mid = (big + small) // 2
+        if big - small <= 1:
+            if check(big): return big
+            else:          return small
         else:
-            return binarySearch(small, mid)
+            if check(mid):
+                return func(mid, big)
+            else:
+                return func(small, mid)
+
+    return func(_small, _big)
+
 
 # 条件を満たす最小の値を返す
-def binarySearch(small, big):
+def binarySearch(_small, _big):
+    def check(i):
+        return True
+
     mid = (big + small) // 2
     if big - small <= 1:
         if check(small): return small
         else:            return big
     else:
         if check(mid):
-            return binarySearch(small, mid)
+            return func(small, mid)
         else:
-            return binarySearch(mid, big)
+            return func(mid, big)
+
+    return func(_small, _big)
--- Math/繰り返し二乗法.py
+++ Math/繰り返し二乗法.py
@@ -2,6 +2,7 @@
 
 MOD = 1000000007
 def repeatSquares(x, n):  # x^n
+    # x = 0 の場合をケアしてないので注意
     res = 1
     while n != 0:
         if n % 2 == 1:
